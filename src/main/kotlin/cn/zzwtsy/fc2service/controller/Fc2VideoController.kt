546cdbe7139d157be8cde8fc6b92c1d429697205
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-01-31 00:58:22 +0800

    add: query api

--- src/main/kotlin/cn/zzwtsy/fc2service/controller/Fc2VideoController.kt
+++ src/main/kotlin/cn/zzwtsy/fc2service/controller/Fc2VideoController.kt
@@ -6,36 +6,73 @@ import cn.zzwtsy.fc2service.model.VideoInfo
 import cn.zzwtsy.fc2service.repository.Fc2VideoInfoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api")
+@RestController
+@RequestMapping("/frontendAPI")
 class Fc2VideoController {
     @Autowired
     private lateinit var fc2VideoInfoRepository: Fc2VideoInfoRepository
 
-    @GetMapping("/getNewVideoInfoByDateDesc")
+    @GetMapping("/getNewVideoInfoListByDateDesc")
     fun getNewVideoInfoByDateDesc(
-        @RequestParam pageIndex: Int,
-        @RequestParam pageSize: Int
+        @RequestParam(required = true) pageIndex: Int,
+        @RequestParam(required = true) pageSize: Int
     ): R<List<VideoInfo>> {
-        if (pageIndex < 1 || pageSize < 1) {
+        if (pageIndex < 0 || pageSize < 1) {
             return R.failure(RCode.PARAM_ERROR)
         }
-        val page = fc2VideoInfoRepository.queryVideoInfoOrderByReleaseDateDesc(pageIndex - 1, pageSize)
-        if (page.content.isEmpty()) {
-            return R.failure(RCode.NO_DATA)
+        val videoInfoPage = fc2VideoInfoRepository.queryVideoInfoListOrderByReleaseDateDesc(pageIndex, pageSize)
+        return if (videoInfoPage.content.isEmpty()) {
+            R.failure(RCode.NO_DATA)
+        } else {
+            R.success(videoInfoPage.content, videoInfoPage.totalPages)
         }
-        return R.success(page.content, page.totalPages)
     }
 
     @GetMapping("/getVideoInfoById")
-    fun getVideoInfoById(@RequestParam fc2Id: Long): R<VideoInfo> {
-        val findByVideoId = fc2VideoInfoRepository.findByVideoId(fc2Id)
-        return if (findByVideoId != null) {
-            R.success(findByVideoId)
+    fun getVideoInfoById(@RequestParam(required = true) fc2Id: Long): R<VideoInfo> {
+        val videoInfo = fc2VideoInfoRepository.findByVideoId(fc2Id)
+        return if (videoInfo != null) {
+            R.success(videoInfo)
         } else {
             R.failure(RCode.NO_DATA)
+        }
+    }
+
+    @GetMapping("/getVideoInfoListByTagId")
+    fun getVideoInfoListByTagId(
+        @RequestParam(required = true) tagId: Long,
+        @RequestParam(required = true) pageIndex: Int,
+        @RequestParam(required = true) pageSize: Int
+    ): R<List<VideoInfo>> {
+        if (pageIndex < 0 || pageSize < 1) {
+            return R.failure(RCode.PARAM_ERROR)
+        }
+        val videoInfoPage = fc2VideoInfoRepository.queryVideoInfoListByTagsId(tagId, pageIndex, pageSize)
+        return if (videoInfoPage.content.isEmpty()) {
+            R.failure(RCode.NO_DATA)
+        } else {
+            R.success(videoInfoPage.content, videoInfoPage.totalPages)
+        }
+    }
+
+    @GetMapping("/getVideoInfoListBySellerId")
+    fun getVideoInfoListBySellerId(
+        @RequestParam(required = true) sellerId: Long,
+        @RequestParam(required = true) pageIndex: Int,
+        @RequestParam(required = true) pageSize: Int
+    ): R<List<VideoInfo>> {
+        if (pageIndex < 0 || pageSize < 1) {
+            return R.failure(RCode.PARAM_ERROR)
+        }
+        val videoInfoPage = fc2VideoInfoRepository.queryVideoInfoByListBySellerId(sellerId, pageIndex, pageSize)
+        return if (videoInfoPage.content.isEmpty()) {
+            R.failure(RCode.NO_DATA)
+        } else {
+            R.success(videoInfoPage.content, videoInfoPage.totalPages)
         }
     }
 }
