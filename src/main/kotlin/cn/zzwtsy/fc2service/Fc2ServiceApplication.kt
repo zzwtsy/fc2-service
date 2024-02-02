b75bb8ba90d15b6fa504e1b346b633a94c8f352d
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-02-02 23:28:02 +0800

    jimmer 开启导出 api 配置

--- src/main/kotlin/cn/zzwtsy/fc2service/Fc2ServiceApplication.kt
+++ src/main/kotlin/cn/zzwtsy/fc2service/Fc2ServiceApplication.kt
@@ -1,11 +1,13 @@
 package cn.zzwtsy.fc2service
 
+import org.babyfish.jimmer.client.EnableImplicitApi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableImplicitApi
 @EnableScheduling
 @EnableAsync
 class Fc2ServiceApplication
