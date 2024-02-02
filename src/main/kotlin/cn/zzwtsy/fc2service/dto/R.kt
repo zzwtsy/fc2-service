f14393bb89249b103498162c0adcd47fccbe7893
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-02-02 23:24:40 +0800

    将分页数据中的总页数改为总条数

--- src/main/kotlin/cn/zzwtsy/fc2service/dto/R.kt
+++ src/main/kotlin/cn/zzwtsy/fc2service/dto/R.kt
@@ -7,7 +7,7 @@ data class R<T>(
     val code: Int, // 返回码
     val message: String, // 返回消息
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    val totalPages: Int? = null, // 总页数（可选）
+    val totalElements: Long? = null, // 总条数（可选）
     @JsonInclude(JsonInclude.Include.NON_NULL)
     val data: T? = null // 数据（可选）
 ) {
@@ -16,11 +16,11 @@ data class R<T>(
          * 创建一个成功的R对象
          *
          * @param data 数据
-         * @param totalPages 总页数
+         * @param totalElements 总条数
          * @return R对象
          */
-        fun <T> success(data: T, totalPages: Int? = null): R<T> {
-            return R(RCode.SUCCESS.code, RCode.SUCCESS.msg, totalPages, data)
+        fun <T> success(data: T, totalElements: Long? = null): R<T> {
+            return R(RCode.SUCCESS.code, RCode.SUCCESS.msg, totalElements, data)
         }
 
         /**
